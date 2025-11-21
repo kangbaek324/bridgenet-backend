57b5447c18fec807342762fe4f8fbf0bc64b7168
kangbaekho <kbh081004@gmail.com>
2025-11-21 11:03:29 +0900

    refactor: 블록체인 연결방식을 하드코딩에서 DB리스트 조회 후 등록으로 변경

--- src/main/java/com/baekho/bridgenet/domain/bridge/controller/BridgeController.java
+++ src/main/java/com/baekho/bridgenet/domain/bridge/controller/BridgeController.java
@@ -1,5 +1,6 @@
 package com.baekho.bridgenet.domain.bridge.controller;
 
+import com.baekho.bridgenet.domain.auth.entity.Users;
 import com.baekho.bridgenet.domain.bridge.dto.RequestOptionSetRequestDTO;
 import com.baekho.bridgenet.domain.bridge.service.BridgeService;
 import com.baekho.bridgenet.global.common.response.SuccessResponse;
@@ -7,6 +8,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,7 +23,10 @@ public class BridgeController {
     public ResponseEntity<SuccessResponse<Void>> setRequestOption(
             @Valid @RequestBody RequestOptionSetRequestDTO dto
     ) {
-        bridgeService.setRequestOptionStatus(dto);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Users user = (Users) authentication.getPrincipal();
+
+        bridgeService.setRequestOptionStatus(dto, user);
 
         return ResponseEntity.ok(new SuccessResponse<>("", null));
     }
