933971adf2b7c0820d85df32b1805c621a7f8ff5
kangbaekho <kbh081004@gmail.com>
2026-01-14 10:05:36 +0900

    refactor: 파일명 수정

--- src/main/java/com/baekho/bridgenet/domain/chain/controller/RpcController.java
+++ src/main/java/com/baekho/bridgenet/domain/chain/controller/RpcController.java
@@ -1,43 +1,46 @@
 package com.baekho.bridgenet.domain.chain.controller;
 
-import com.baekho.bridgenet.domain.chain.dto.request.RpcAddRequestDTO;
 import com.baekho.bridgenet.domain.chain.dto.request.RpcUpdateRequestDTO;
-import com.baekho.bridgenet.domain.chain.dto.response.RpcAddResponseDTO;
 import com.baekho.bridgenet.domain.chain.dto.response.RpcResponseDTO;
 import com.baekho.bridgenet.domain.chain.dto.response.RpcUpdateResponseDTO;
-import com.baekho.bridgenet.domain.chain.service.ChainRpcService;
-import com.baekho.bridgenet.global.common.enums.Protocol;
+import com.baekho.bridgenet.domain.chain.service.RpcService;
 import com.baekho.bridgenet.global.common.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/chains/{chainId}/rpcs")
+@RequestMapping("/api/rpcs")
 public class RpcController {
-    private final ChainRpcService chainRpcService;
+    private final RpcService rpcService;
 
-    @GetMapping
-    public ResponseEntity<SuccessResponse<List<RpcResponseDTO>>> getRpcs(
-            @PathVariable Long chainId,
-            @RequestParam(required = false) Protocol protocol
+    @GetMapping("/{id}")
+    public ResponseEntity<SuccessResponse<RpcResponseDTO>> getRpc(
+            @PathVariable Long id
     ) {
-        List<RpcResponseDTO> res = chainRpcService.getRpcs(chainId, protocol);
+        RpcResponseDTO res = rpcService.getRpc(id);
 
         return ResponseEntity.ok(new SuccessResponse<>("", res));
     }
 
-    @PostMapping
+    @PutMapping("/{id}")
     @PreAuthorize("@authService.isAdmin(principal)")
-    public ResponseEntity<SuccessResponse<RpcAddResponseDTO>> addRpc(
-            @RequestBody RpcAddRequestDTO dto,
-            @PathVariable Long chainId
+    public ResponseEntity<SuccessResponse<RpcUpdateResponseDTO>> updateRpc(
+            @RequestBody RpcUpdateRequestDTO dto,
+            @PathVariable Long id
     ) {
-        RpcAddResponseDTO res = chainRpcService.addRpc(dto, chainId);
+        RpcUpdateResponseDTO res = rpcService.updateRpc(dto, id);
         return ResponseEntity.ok(new SuccessResponse<>("", res));
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("@authService.isAdmin(principal)")
+    public ResponseEntity<SuccessResponse<Void>> deleteRpc(
+            @PathVariable Long id
+    ) {
+        rpcService.deleteRpc(id);
+        return ResponseEntity.ok(new SuccessResponse<>("", null));
     }
 }
